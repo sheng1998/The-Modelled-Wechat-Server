785d47063e624f0b61b80d9885004ef41a215201
z <18475140601@163.com>
2022-08-07 17:50:18 +0800

    refactor: 重构用户名、密码校验规则

--- app.js
+++ app.js
@@ -11,32 +11,40 @@ module.exports = (app) => {
   // 校验用户名
   // eslint-disable-next-line consistent-return
   app.validator.addRule('username', (rule, value) => {
-    if (typeof value !== 'string') {
-      return '用户名必需为字符串！';
-    } if (/\s+/.test(value)) {
-      return '用户名禁止携带空格！';
-    } if (value.length < 2 || value.length > 15) {
-      return '用户名长度限制在2-15字符之间！';
-    } if (/[\\/]+/.test(value)) {
-      return '用户名禁止携带斜杠！';
+    let message = '';
+    if (!value) {
+      message = '请输入用户名！';
+    } else if (typeof value !== 'string') {
+      message = '用户名必需为字符串！';
+    } else if (/\s+/.test(value)) {
+      message = '用户名禁止携带空格！';
+    } else if (value.length < 2 || value.length > 15) {
+      message = '用户名长度限制在2-15字符之间！';
+    } else if (/[\\/]+/.test(value)) {
+      message = '用户名禁止携带斜杠！';
     }
+    if (message) return message;
   });
 
   // 校验密码
   // eslint-disable-next-line consistent-return
   app.validator.addRule('password', (rule, value) => {
-    if (typeof value !== 'string') {
-      return '密码必需为字符串！';
-    } if (/\s+/.test(value)) {
-      return '密码禁止携带空格！';
-    } if (/[`‘“，。、；：？！【】《》（）\u4e00-\u9fa5]+/.test(value)) {
-      return '密码禁止含有中文或中文字符！';
-    } if (value.length < 8 || value.length > 30) {
-      return '密码长度限制在8-30字符之间！';
-    } if (/[\\/<>[](){}]/.test(value)) {
-      return '密码禁止携带斜杠或括号！';
-    } if (!(/[a-zA-Z]+/.test(value) && /\d+/.test(value))) {
-      return '密码强度太低，必需含有字母和数字！';
+    let message = '';
+    if (!value) {
+      message = '请输入密码！';
+    } else if (typeof value !== 'string') {
+      message = '密码必需为字符串！';
+    } else if (/\s+/.test(value)) {
+      message = '密码禁止携带空格！';
+    } else if (/[‘’“”，。、；：？！【】《》（）\u4e00-\u9fa0]/.test(value)) {
+      message = '密码禁止含有中文或中文字符！';
+    } else if (value.length < 8 || value.length > 30) {
+      message = '密码长度限制在8-30字符之间！';
+    } else if (/[\\/]/.test(value)) {
+      message = '密码禁止携带斜杠！';
+    } else if (!(/([a-z].?\d)|(\d.?[a-z])/i.test(value))) {
+      message = '密码必需含有字母和数字！';
     }
+    if (message) return message;
   });
 };
